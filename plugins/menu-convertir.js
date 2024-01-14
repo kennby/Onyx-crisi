c10b01eb726070c985dc450cfd8523553fe36488
kennby <153956421+kennby@users.noreply.github.com>
2024-01-14 12:35:19 -0500

    Update menu-convertir.js

--- plugins/menu-convertir.js
+++ plugins/menu-convertir.js
@@ -92,7 +92,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   ▸🃏 _${usedPrefix}stickermarker *<efecto> <imagen>*_
   ▸🃏 _${usedPrefix}stickerfilter *<efecto> <imagen>*_
 *╰───── • ◆ • ─────┅┅┄┄*
-*_BY: ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦_*
+*_BY: 𝗢𝗡𝗬𝗫_*
 `.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
