8944eb3636c838b705af7555cd7d5799f19512ac
kennby <153956421+kennby@users.noreply.github.com>
2024-01-14 12:11:54 -0500

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,16 +1,16 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
-   const res = await fetch('https://api.github.com/repos/WorkCwp/Criwilop-Bot-MD');
+   const res = await fetch('https://api.github.com/repos/kenn/bennbyx-MD');
    const json = await res.json();
    let txt = `           *🍁  S C R I P T  🍁*\n\n`;
-      txt += `◦  *Nombre:* ${json?.name || '♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦'}\n`;
+      txt += `◦  *Nombre:* ${json?.name || '𝗢𝗡𝗬𝗫'}\n`;
       txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
       txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
       txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
       txt += `◦  *Url:* ${json?.html_url || 'https://github.com/kennx/onyxbyx'}\n\n`;
       txt += `	 ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
-   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://atom.bio/criwilop`, "sourceUrl": `https://atom.bio/criwilop`}}}, {quoted: m});
+   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/onyx-bot`, "sourceUrl": `https://www.atom.bio/onyx-bot`}}}, {quoted: m});
 };
 handler.help = ['scbot'];
 handler.tags = ['info'];
