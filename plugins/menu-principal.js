e54e59ac244326e507fa1baca8557193e5554e64
kennby <153956421+kennby@users.noreply.github.com>
2024-01-14 12:29:04 -0500

    Update menu-principal.js

--- plugins/menu-principal.js
+++ plugins/menu-principal.js
@@ -56,7 +56,7 @@ ${readMore}
   ∙ Herramientas ◃──▹ menu11
   ∙ Economía ◃─────▹ menu12
 *╰───── • ◆ • ─────┅┅┄┄*
-*_BY: ♰⍣𝑪𝒓𝒊𝒘𝒊𝒍𝒐𝒑𝑩𝒐𝒕-𝑴𝑫࿐⛦_*`.trim();
+*_BY: 𝗢𝗡𝗬𝗫_*`.trim();
 
     if (m.isGroup) {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net',};
